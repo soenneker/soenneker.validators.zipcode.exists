8a82d75bd100ab85a5b303276f0fad12d5697795
Jake Soenneker <jake@soeneker.com>
2025-05-26 23:38:45 +0000

    FluentAssertions -> AwesomeAssertions namespace

--- test/Soenneker.Validators.ZipCode.Exists.Tests/ZipCodeExistsValidatorTests.cs
+++ test/Soenneker.Validators.ZipCode.Exists.Tests/ZipCodeExistsValidatorTests.cs
@@ -1,5 +1,5 @@
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Soenneker.Validators.ZipCode.Exists.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
