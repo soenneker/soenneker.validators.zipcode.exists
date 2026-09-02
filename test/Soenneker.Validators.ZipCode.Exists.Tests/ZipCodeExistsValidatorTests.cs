c82571c4bbbe9bdab3f72882846cffd98989d6fb
soenneker <jake@soenneker.com>
2026-09-02 14:22:09 -0500

    Add cancellation tokens to async tests

--- test/Soenneker.Validators.ZipCode.Exists.Tests/ZipCodeExistsValidatorTests.cs
+++ test/Soenneker.Validators.ZipCode.Exists.Tests/ZipCodeExistsValidatorTests.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Threading.Tasks;
 using AwesomeAssertions;
 using Soenneker.Validators.ZipCode.Exists.Abstract;
@@ -17,28 +18,28 @@ public class ZipCodeExistsValidatorTests : HostedUnitTest
     }
 
     [Test]
-    public async Task Validate_ValidZipCode_ReturnsTrue()
+    public async Task Validate_ValidZipCode_ReturnsTrue(CancellationToken cancellationToken)
     {
         const string validZipCode = "00611";
-        bool result = await _validator.Validate(validZipCode, System.Threading.CancellationToken.None);
+        bool result = await _validator.Validate(validZipCode, cancellationToken);
 
         result.Should().BeTrue();
     }
 
     [Test]
-    public async Task Validate_LongZipCode_ReturnsTrue()
+    public async Task Validate_LongZipCode_ReturnsTrue(CancellationToken cancellationToken)
     {
         const string longZipCode = "00611-5353";
-        bool result = await _validator.Validate(longZipCode, System.Threading.CancellationToken.None);
+        bool result = await _validator.Validate(longZipCode, cancellationToken);
 
         result.Should().BeTrue();
     }
 
     [Test]
-    public async Task Validate_InvalidZipCode_ReturnsFalse()
+    public async Task Validate_InvalidZipCode_ReturnsFalse(CancellationToken cancellationToken)
     {
         const string validZipCode = "12345";
-        bool result = await _validator.Validate(validZipCode, System.Threading.CancellationToken.None);
+        bool result = await _validator.Validate(validZipCode, cancellationToken);
 
         result.Should().BeFalse();
     }
